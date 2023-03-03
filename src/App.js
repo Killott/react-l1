5806e0ae2088744e6e422599646e2bbfbf61e8ff
Kirill Filatov <filatov@keeneye.pro>
2023-03-03 21:33:42 +0300

    Удилил автогенерируемое приложение

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React, {useState} from "react";
+import Counter from "./components/Counter";
+import ClassConter from "./components/ClassConter";
 
 function App() {
 
-    const [count, setCount] = useState(0);
-    const [value, setValue] = useState('TEXT')
-
-    function increment() {
-        setCount(count + 1)
-    }
-
-    function decrement() {
-        setCount(count - 1)
-    }
-
     return (
         <div className="App">
-            <h2>{count}</h2>
-            <h2>{value}</h2>
-            <input
-                type="text"
-                value={value}
-                onChange={event => setValue(event.target.value)}
-            />
-            <button onClick={increment}>Увеличить</button>
-            <button onClick={decrement}>Уменьшить</button>
+            <ClassConter></ClassConter>
+
         </div>
     );
 }
